49fe7a0d555b6997bf879335eba6a324315b5237
Aimen Munawar <aimenmunawarofficial@gmail.com>
2025-05-13 00:43:36 +0500

    improved http headers

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import helmet from "helmet";
 
 // App Config
 
@@ -19,6 +20,7 @@ connectCloudinary();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 // api endpoints
 
